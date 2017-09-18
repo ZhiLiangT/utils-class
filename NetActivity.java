bbbfcd3cc6aa87aaa02da2522937deacb81a59df
ZhiLiangT <15036305782@163.com>
2017-09-18 18:58:06 +0800

    网络工具类

---
+++ NetActivity.java
@@ -0,0 +1,15 @@
+package com.tianzl.webview_test.test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.tianzl.webview_test.R;
+
+public class NetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_net);
+    }
+}
